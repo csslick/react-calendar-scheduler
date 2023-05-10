a8603adc2d1aeffedb53de1fcfa91df9e01d5f8f
csslick <digitone@naver.com>
2023-05-10 20:31:19 +0900

    111

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,8 @@
-import { useState } from 'react'
 import './App.css'
 import Calendar from './components/Calendar'
 
 function App() {
-  const [count, setCount] = useState(0)
-
+ 
   return (
     <div className='App py-4'>
       <h1 style={{fontSize: '30px'}}>Calendar 일정</h1>
