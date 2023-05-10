75e8a56935051bb731745caac8b19264b519b422
csslick <digitone@naver.com>
2023-05-10 20:24:53 +0900

    111

--- src/components/Calendar.tsx
+++ src/components/Calendar.tsx
@@ -1,4 +1,5 @@
 import { useState, useRef } from 'react';
+import Infobox from './Infobox';
 
 const Calendar = () => {
   const today = new Date();
@@ -11,8 +12,8 @@ const Calendar = () => {
   ]);
 
   interface DataItem {
-    id: string | undefined;
-    content: string | undefined;
+    id: string ;
+    content: string;
   }
 
   const [todayData, setTodayData] = useState<DataItem | null>(null); 
@@ -63,6 +64,7 @@ const Calendar = () => {
     <div className='mt-4'>
       <button className='btn btn-dark' onClick={goToPreviousMonth}>이전달</button>&nbsp;
       <button className='btn btn-dark' onClick={goToNextMonth}>다음달</button>
+
       <table className='table mt-4 caption-top'>
         <caption>{currentYear}년 <b>{currentMonth + 1}월</b></caption>
         <thead>
@@ -106,58 +108,13 @@ const Calendar = () => {
         </tbody>
       </table>
 
-      <div className='info-box border p-3'>
-        <p>{saveKey ? saveKey : '오늘의 스케쥴'}</p>
-        <p>{todayData ? todayData.content : '일정없음'}</p>
-        <div className="input-group">
-          <input 
-            ref={inputRef}
-            className="form-control" 
-            type="text" placeholder='여기에 일정 입력'/>
-          <button 
-            onClick={() => {
-              // 날짜 선택 확인
-              if(saveKey) {
-                // 입력 확인
-                if(inputRef.current?.value !== '') {
-                  console.log(inputRef.current?.value);
-                  const isSameData = data.find(item => {
-                    return item.id === saveKey
-                  })
-                  // 같은 키의 자료가 없으면 일정 저장
-                  if(!isSameData) {
-                    let copy:any = [...data, {
-                      id: saveKey,
-                      content: inputRef.current?.value 
-                    }]
-                    setData(copy);
-                    // 스케쥴 표시 업데이트
-                    setTodayData({
-                      id: saveKey, 
-                      content: inputRef.current?.value 
-                    });             
-                  }
-                  console.log('isSameData = ', isSameData)
-                }
-              } else {
-                alert('날짜를 선택해주세요')
-              }
-            }} 
-            className="btn btn-primary">입력</button>
-        </div>
-        <h3 className='mt-4'>전체일정</h3>
-        <ul className='list-group'>
-        {
-          data.reverse().map((item) => {
-            return (
-              <li 
-                className='list-group-item' 
-                key={item.id}>{item.id}: {item.content}</li>
-            )
-          })
-        }
-        </ul>
-      </div>
+      <Infobox 
+        data={data} 
+        setData={setData} 
+        setTodayData={setTodayData as any} 
+        saveKey={saveKey} 
+        todayData={todayData} 
+        inputRef={inputRef} />
       
     </div>
   );
